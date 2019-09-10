1f7a51f720971e7d72232293bab12ff84d49a0af
ctobiasz <tobiasz.christopher@gmail.com>
2019-09-09 22:21:20 -0400

    I couldn't help myself. I had to add a title icon. Besides that just auto indent on all files to make it look puurty and quickneasy php bypass hack to delopy on heroku

--- main.js
+++ main.js
@@ -17,32 +17,31 @@ const no_access = function(){
 }
 
 const over21 = function(){
-        message_alert.style.visibility = "visible";
-        message_alert.style.backgroundColor = "rgb(0, 0, 0, 0.6)";
-        message_alert.style.color = "white";
-        message_alert.innerHTML = "Welcome to Synthwave Brew."
-        message_alert.style.boxShadow = "0 0 20px black";
-            setTimeout(
-                    function(){
-                    message_alert.style.visibility = "hidden";
-                  }, 3000);
-}
+  message_alert.style.visibility = "visible";
+  message_alert.style.backgroundColor = "rgb(0, 0, 0, 0.6)";
+  message_alert.style.color = "white";
+  message_alert.innerHTML = "Welcome to Synthwave Brew."
+  message_alert.style.boxShadow = "0 0 20px black";
+  setTimeout(
+    function(){
+      message_alert.style.visibility = "hidden";
+    }, 3000);
+  }
 
-const ageGate = function(){
-  let dateYear = document.querySelector("#date-year").value;
-  let current_age = current_year - dateYear;
-  if(current_age < 21) {
-    for(let i=0; i<input_dates.length; i++){
-      input_dates[i].disabled = true;
-    };
-    // subBtn.style.display= "none"; MAYBE I"LL INCLUDE THIS *****
-  no_access();
-  } else {
-    document.cookie = "overtwentyone=true; expires=Thu, 01 Jan 3000 00:00:00 UTC; path=/";
-        over21();
+  const ageGate = function(){
+    let dateYear = document.querySelector("#date-year").value;
+    let current_age = current_year - dateYear;
+    if(current_age < 21) {
+      for(let i=0; i<input_dates.length; i++){
+        input_dates[i].disabled = true;
+      };
+      no_access();
+    } else {
+      document.cookie = "overtwentyone=true; expires=Thu, 01 Jan 3000 00:00:00 UTC; path=/";
+      over21();
     }
-};
+  };
 
-subBtn.addEventListener("click", ageGate);
-if(document.cookie.includes('overtwentyone=true')){
+  subBtn.addEventListener("click", ageGate);
+  if(document.cookie.includes('overtwentyone=true')){
     over21()};
